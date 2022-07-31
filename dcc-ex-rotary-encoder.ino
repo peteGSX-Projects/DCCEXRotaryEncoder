8d49a54f9a4b59d595e1ee089cca9b5df11b6e75
peteGSX <peteracole@outlook.com.au>
2022-08-01 07:04:16 +1000

    Basic function enabled without OLED

--- dcc-ex-rotary-encoder.ino
+++ dcc-ex-rotary-encoder.ino
@@ -17,16 +17,36 @@
 #include "Wire.h"
 #include "version.h"
 
+/*
+Global variable to allow rotation of the encoder back to "home"
+without updating the position send to the CS.
+*/
 bool encoderRead = true;
 
-// Rotary encoder is wired with the common to ground and the two
-// outputs to pins 5 and 6.
+/*
+Instantiate our rotary encoder object.
+Rotary encoder is wired with the common to ground and the two
+outputs as defined in config.h.
+*/
 Rotary rotary = Rotary(ROTARY_DT, ROTARY_CLK);
 
+/*
+Instantiate our rotary encoder button.
+Rotary encoder button wired as defined in config.h.
+*/
 Switch encoderButton(ROTARY_BTN, INPUT_PULLUP);
 
-// Counter that will be incremented or decremented by rotation.
-int counter = 0;
+/*
+Global variable for the counter that will be incremented or
+decremented by rotation.
+*/
+int8_t counter = 0;
+
+/*
+Global variable that is set by the encoder button single push
+and is sent to the CS when requested.
+*/
+int8_t position = 0;
 
 void setup() {
   Serial.begin(115200);
@@ -35,7 +55,6 @@ void setup() {
   Serial.print(F("Available at I2C address 0x"));
   Serial.println(I2C_ADDRESS, HEX);
   Wire.begin(I2C_ADDRESS);
-  Wire.onReceive(receiveEvent);
   Wire.onRequest(requestEvent);
 }
 
@@ -45,31 +64,35 @@ void loop() {
   if (encoderButton.longPress()) {
     // Disable reading position allow rotation to "home"
     encoderRead = false;
-    Serial.println(F("Disabling position movement"));
+    Serial.println(F("Disabling position counts"));
   } else if (encoderButton.singleClick() && encoderRead) {
-    // Send rotary position here
+    position = counter;
+    Serial.print(F("Sending position "));
+    Serial.print(position);
+    Serial.println(F(" to CommandStation"));
   } else if (encoderButton.singleClick() && !encoderRead) {
     // Once rotated to "home", zero counter and enable again
     counter = 0;
     encoderRead = true;
+    Serial.println(F("Enabling position counts"));
   }
 
   if (encoderRead) {
     unsigned char result = rotary.process();
     if (result == DIR_CW) {
-      counter++;
-      Serial.println(counter);
+      if (counter < 127) {
+        counter++;
+        Serial.println(counter);
+      }
     } else if (result == DIR_CCW) {
-      counter--;
-      Serial.println(counter);
+      if (counter > -127) {
+        counter--;
+        Serial.println(counter);
+      }
     }
   }
 }
 
-void receiveEvent(int received) {
-
-}
-
 void requestEvent() {
-
+  Wire.write(position);
 }
