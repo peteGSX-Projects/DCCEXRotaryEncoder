2b9e30f9d2b2d9680c860db1cef17beec52932f8
peteGSX <peteracole@outlook.com.au>
2022-08-12 07:06:04 +1000

    Add initial OLED display

--- Rotary.h
+++ Rotary.h
@@ -15,20 +15,6 @@
 
 #include "Arduino.h"
 
-// Enable this to emit codes twice per step.
-//#define HALF_STEP
-
-// Enable weak pullups
-#define ENABLE_PULLUPS
-
-// Values returned by 'process'
-// No complete step yet.
-#define DIR_NONE 0x0
-// Clockwise step.
-#define DIR_CW 0x10
-// Anti-clockwise step.
-#define DIR_CCW 0x20
-
 class Rotary
 {
   public:
--- dcc-ex-rotary-encoder.ino
+++ dcc-ex-rotary-encoder.ino
@@ -1,5 +1,17 @@
 /*
  *  © 2022 Peter Cole
+ *
+ *  This is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  It is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  See <https://www.gnu.org/licenses/>.
 */
 
 #include <Arduino.h>
@@ -16,6 +28,9 @@
 #include "Rotary.h"
 #include "Wire.h"
 #include "version.h"
+#include <SPI.h>
+#include "SSD1306Ascii.h"
+#include "SSD1306AsciiSpi.h"
 
 /*
 Global variable to allow rotation of the encoder back to "home"
@@ -48,14 +63,67 @@ and is sent to the CS when requested.
 */
 int8_t position = 0;
 
+/*
+Instantiate our OLED object.
+*/
+SSD1306AsciiSpi oled;
+
+/*
+Function to display the currently selected position of the encoder.
+Set this by a button press, or 0 on startup.
+*/
+void displaySelectedPosition(int8_t position) {
+  oled.setCursor(0, 0);
+  oled.set2X();
+  oled.print(F("Current: "));
+  oled.print(position);
+  oled.clearToEOL();
+}
+
+/*
+Function to display the position the encoder is moved to.
+This should be updated by any encoder movement.
+*/
+void displayNewPosition(int8_t position) {
+  oled.setCursor(0, 3);
+  oled.set2X();
+  oled.print(F("Move to: "));
+  oled.print(position);
+  oled.clearToEOL();
+}
+
+/*
+Function to display the home reset process.
+This should be set by a long press.
+*/
+void displayHomeReset() {
+  oled.clear();
+  oled.set1X();
+  oled.setCursor(0,0);
+  oled.println(F("Resetting home position"));
+  oled.println(F("Rotate encoder to home"));
+  oled.println(F("Press button to confirm"));
+}
+
 void setup() {
   Serial.begin(115200);
+  oled.begin(&SH1106_128x64, CS_PIN, DC_PIN);
+  oled.setFont(Callibri11);
+  oled.clear();
   Serial.print(F("DCC-EX Rotary Encoder "));
   Serial.println(VERSION);
   Serial.print(F("Available at I2C address 0x"));
   Serial.println(I2C_ADDRESS, HEX);
+  oled.println(F("DCC-EX Rotary Encoder"));
+  oled.print(F("Version: "));
+  oled.println(VERSION);
+  oled.print(F("I2C Address: 0x"));
+  oled.println(I2C_ADDRESS, HEX);
   Wire.begin(I2C_ADDRESS);
   Wire.onRequest(requestEvent);
+  delay(2000);
+  oled.clear();
+  displaySelectedPosition(counter);
 }
 
 void loop() {
@@ -65,16 +133,19 @@ void loop() {
     // Disable reading position allow rotation to "home"
     encoderRead = false;
     Serial.println(F("Disabling position counts"));
+    displayHomeReset();
   } else if (encoderButton.singleClick() && encoderRead) {
     position = counter;
     Serial.print(F("Sending position "));
     Serial.print(position);
     Serial.println(F(" to CommandStation"));
+    displaySelectedPosition(position);
   } else if (encoderButton.singleClick() && !encoderRead) {
     // Once rotated to "home", zero counter and enable again
     counter = 0;
     encoderRead = true;
     Serial.println(F("Enabling position counts"));
+    displaySelectedPosition(position);
   }
 
   if (encoderRead) {
@@ -82,14 +153,16 @@ void loop() {
     if (result == DIR_CW) {
       if (counter < 127) {
         counter++;
-        Serial.println(counter);
       }
     } else if (result == DIR_CCW) {
       if (counter > -127) {
         counter--;
-        Serial.println(counter);
       }
     }
+#ifdef DIAG
+    Serial.println(counter);
+#endif
+    displayNewPosition(counter);
   }
 }
 
